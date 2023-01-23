afe849bf64926eb3616fcbebea69caff26bf670c
Carolina Ramirez Trillo <caroramireztrillo@gmail.com>
2023-01-23 16:24:37 -0300

    traer los push

--- app/controllers/medical_centers_controller.rb
+++ app/controllers/medical_centers_controller.rb
@@ -1,2 +1,10 @@
 class MedicalCentersController < ApplicationController
+
+  def index
+    @exams = params[:exams]
+    @habits = params[:habits]
+    @bookings = params[:bookings]
+    @recipes = params[:recipes]
+  end
+
 end
