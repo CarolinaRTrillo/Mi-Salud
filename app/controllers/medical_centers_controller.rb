0afd2ec24cf662d3439fd5e20c1ba5994e055ed9
Carolina Ramirez Trillo <caroramireztrillo@gmail.com>
2023-01-24 00:02:45 -0300

    cambio en la navbar, quienes somos y home2

--- app/controllers/medical_centers_controller.rb
+++ app/controllers/medical_centers_controller.rb
@@ -1,10 +1,8 @@
 class MedicalCentersController < ApplicationController
-
   def index
-    @exams = params[:exams]
-    @habits = params[:habits]
-    @bookings = params[:bookings]
-    @recipes = params[:recipes]
+    @exams = current_user.exams.take(3)
+    @habits = current_user.habits.take(3)
+    @bookings = current_user.bookings.take(3)
+    @recipes = current_user.recipes.take(3)
   end
-
 end
