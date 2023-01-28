717913f87ba5a26fceab067ff85b95f072d2304f
Carolina Ramirez Trillo <caroramireztrillo@gmail.com>
2023-01-28 18:03:28 -0300

    Ultimos cambios

--- app/models/medical_center.rb
+++ app/models/medical_center.rb
@@ -1,6 +1,10 @@
 class MedicalCenter < ApplicationRecord
-  @exams = current_user.exams.take(3)
-  @habits = current_user.habits
-  @recipes = current_user.recipes.take(3)
-  @bookings = current_user.bookings
+  # helper_method :current_user
+  # if current_user
+    # raise
+    # @exams = current_user.exams.take(3)
+    # @habits = current_user.habits
+    # @recipes = current_user.recipes.take(3)
+    # @bookings = current_user.bookings
+  # end
 end
