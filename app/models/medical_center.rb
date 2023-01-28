e9f89570ef7f430e9050c01a0554bf9024abc9ea
Carolina Ramirez Trillo <caroramireztrillo@gmail.com>
2023-01-28 17:27:44 -0300

    cambio para heroku

--- app/models/medical_center.rb
+++ app/models/medical_center.rb
@@ -1,6 +1,8 @@
 class MedicalCenter < ApplicationRecord
-  @exams = current_user.exams.take(3)
-  @habits = current_user.habits
-  @recipes = current_user.recipes.take(3)
-  @bookings = current_user.bookings
+  if current_user
+    @exams = current_user.exams.take(3)
+    @habits = current_user.habits
+    @recipes = current_user.recipes.take(3)
+    @bookings = current_user.bookings
+  end
 end
