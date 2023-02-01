074e9709e87ad8e4e9a67d855dac48577ea8d564
Francisco Giacchi <franciscogiacchi@outlook.com>
2023-01-31 23:57:16 -0300

    Cambios varios en el front

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,29 +1,35 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
-  }
+    markers: Array,
+  };
 
   connect() {
-    console.log("probando map controller")
-    mapboxgl.accessToken = this.apiKeyValue
+    console.log("probando map controller");
+    mapboxgl.accessToken = this.apiKeyValue;
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
-    })
-    this.#addMarkersToMap()
+      style: "mapbox://styles/mapbox/streets-v10",
+    });
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
   }
 
   #addMarkersToMap() {
-    console.log(this.markersValues)
+    console.log(this.markersValues);
     this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(this.map)
-    })
+      new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(this.map);
+    });
   }
 
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
+  }
 }
